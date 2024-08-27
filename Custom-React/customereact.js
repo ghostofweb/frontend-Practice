5b2224fbf58f643aed2ea4052218636c939a1abd
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-08-28 00:51:17 +0530

    props

--- Custom-React/customereact.js
+++ Custom-React/customereact.js
@@ -1,9 +1,9 @@
 function customeRender(reactElement,container){
-    // const domElement = document.createElement(reactElement.type) //creating elemet of a
-    // domElement.innerHTML=reactElement.children //innerhtml , giving the text
-    // domElement.setAttribute('href',reactElement.props.href) //giving attr of href
-    // domElement.setAttribute("target",reactElement.props.target) //giving attr of href
-    // container.appendChild(domElement)
+    const domElement = document.createElement(reactElement.type) //creating elemet of a
+    domElement.innerHTML=reactElement.children //innerhtml , giving the text
+    domElement.setAttribute('href',reactElement.props.href) //giving attr of href
+    domElement.setAttribute("target",reactElement.props.target) //giving attr of href
+    container.appendChild(domElement)
 }
 // making custome react elemet
 const reactElement = {
