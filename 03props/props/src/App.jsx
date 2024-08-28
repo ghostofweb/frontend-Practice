9c5f7fff85ce5c1e20ddf57829d18497e61c4e2a
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-08-28 13:45:32 +0530

    props and default value

--- 03props/props/src/App.jsx
+++ 03props/props/src/App.jsx
@@ -6,11 +6,17 @@ import Card from './components/card'
 function App() {
   const [count, setCount] = useState(0)
 
+  const user ={
+    name: 'John Doe',
+    age:"92"
+  }
+
   return (
     <>
-    
+    // sending them the properties to the Card
     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
-    <Card/>
+    <Card username="sahiljeet" btntxt="clickme"/>
+    <Card username="singh" />
     </>
   )
 }
