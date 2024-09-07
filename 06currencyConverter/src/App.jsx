285fca17a7ffc6690f013b41839ab37073d69cc2
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2024-09-07 20:28:08 +0530

    a

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css';
 import { Input } from './components';  // Adjusted import to match provided component
 import useCurrencyInfo from './hooks/useCurrencyInfo';
 
+
 function App() {
   const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("inr");
   const [to, setTo] = useState("usd");
   const [convertedAmount, setConvertedAmount] = useState(0);  
-
+  
   const currencyInfo = useCurrencyInfo(from);
   const options = Object.keys(currencyInfo);
 
